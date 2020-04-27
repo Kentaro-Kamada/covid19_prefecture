81dd123667bfc6f1e86ae0c0fae700f100fe04e5
Kentaro Kamada <kamaken0705@gmail.com>
2020-04-27 13:06:10 +0900

    fix codes

--- データ統合.R
+++ データ統合.R
@@ -31,60 +31,69 @@ print_all <- function(x){
 
 # 各都道府県のデータ取得 -------------------------------------------------------------
 
-# # 北海道（修理中） ---------------------------------------------------------
-# 
-# scrape_res <- read_html('http://www.pref.hokkaido.lg.jp/hf/kth/kak/hasseijoukyou.htm', encoding = 'UTF-8')
-# 
-# # data <-
-#   scrape_res %>% 
-#   html_node(xpath = '//*[@id="rs_contents"]/span/table') %>% 
-# 
-#     
-#     
-#     
-#     
-#   html_table(fill = TRUE) %>% 
-#   as_tibble() %>% 
-#   # 全てNAの列を削る
-#   filter(rowSums(is.na(.)) != ncol(.)) %>% 
-#   # 全角を半角に
-#   mutate_all(stringi::stri_trans_nfkc) %>% 
-#   # 列名を埋める，ついでにいらない行を落とす
-#   select(ID = 1, 公表日 = 2, 年代 = 3, 性別 = 4, 居住地 = 5) %>% 
-#   # 1行目（もともとの列名）を落とす
-#   slice(-1) %>% 
-#   # 公表日のクリーニング
-#   separate(col = 公表日, into = c('月', '日'), sep = '/' ) %>% 
-#   mutate(年 = '2020',
-#           月 = str_pad(string = 月, width = 2, pad = '0', side = 'left'),
-#           日 = str_pad(string = 日, width = 2, pad = '0', side = 'left')) %>% 
-#   unite(col = 公表日, c(年, 月, 日), sep = '-') %>% 
-#   # 年代のクリーニング
-#   mutate(年代 = case_when(年代 == '10歳未満' ~ '0代',
-#                           grepl(x = 年代, '代') ~ 年代,
-#                           TRUE ~ NA_character_)) %>% 
-#   # 性別のクリーニング
-#   mutate(性別 = case_when(性別 %in% c('男', '女') ~ paste0(性別, '性'),
-#                           TRUE ~ NA_character_)) %>% 
-#   # 居住地のクリーニング
-#   mutate(居住地 =  gsub(x = 居住地, 
-#                      pattern = '(管内|\\(.+\\)|\\s.+死亡|\\s*\\r\\n\\s*)', 
-#                      replacement = '') %>% 
-#               sub(x = ., pattern  = '総合総合', replacement = '総合')) %>% 
-#     mutate(居住地 = case_when(grepl(x = 居住地, '(札幌市|振興局)') ~ 居住地,
-#                            居住地 == '非公表' ~ NA_character_,
-#                            TRUE ~ '県外')) %>% 
-#   # 型変換
-#   mutate(ID = parse_double(ID),
-#          公表日 = parse_date(公表日)) %>% 
-#   # 県名を付与
-#   mutate(都道府県 = '北海道') %>% select(都道府県, everything()) %>% 
-#   arrange(desc(ID))
-# 
-# # 保存
-# write_excel_csv(x = data, path = 'data/covid19_Hokkaido.csv')
-# 
-# 
+# 北海道 ---------------------------------------------------------
+
+data <-
+  read_csv('https://www.harp.lg.jp/opendata/dataset/1369/resource/2828/patients.csv',
+         locale = locale(encoding = 'CP932')) %>% 
+  select(ID = 1, 公表日 = 2, 年代, 性別, 居住地) %>% 
+  
+  # 年代のクリーニング
+  # とりあえず全角が含まれているので半角に
+  mutate(年代 = stringi::stri_trans_nfkc(年代)) %>% 
+  mutate(年代 = case_when(年代 %in% c('高齢者', '非公表') ~ NA_character_,
+                          年代 == '10歳未満' ~ '0代',
+                          TRUE ~ 年代)) %>%
+  
+  # 性別のクリーニング
+  mutate(性別 = case_when(性別 == '非公表' ~ NA_character_,
+                          TRUE ~ 性別)) %>% 
+  
+  # 居住地のクリーニング
+  mutate(居住地 = case_when(居住地 == '非公表' ~ NA_character_,
+                            str_detect(居住地, '(中国武漢市|[都府県]$)') ~ '県外',
+                            str_detect(居住地, '管?内\\s*$') ~ str_remove(居住地, '管?内\\s*$'),
+                            str_detect(居住地, '(夕張市|岩見沢市|美唄市|芦別市|赤平市|三笠市|滝川市|砂川市|歌志内市|深川市|南幌町|奈井江町|上砂川町|由仁町|長沼町|栗山町|月形町|浦臼町|新十津川町|妹背牛町|秩父別町|雨竜町|北竜町|沼田町)'
+                                          ) ~ '空知総合振興局',
+                            str_detect(居住地, '(旭川市|名寄市|富良野市|士別市|鷹栖町|東神楽町|当麻町|比布町|愛別町|上川町|東川町|美瑛町|上富良野町|中富良野町|南富良野町|和寒町|剣淵町|下川町|美深町|中川町|幌加内町|音威子府村|占冠村)'
+                                          ) ~ '上川総合振興局',
+                            str_detect(居住地, '(江別市|千歳市|恵庭市|石狩市|北広島市|当別町|新篠津村)'
+                                          ) ~ '石狩振興局',
+                            str_detect(居住地, '(小樽市|島牧村|寿都町|黒松内町|蘭越町|ニセコ町|真狩村|留寿都村|喜茂別町|京極町|倶知安町|共和町|岩内町|泊村|神恵内村|積丹町|古平町|仁木町|余市町|赤井川村)'
+                                          ) ~ '後志総合振興局',
+                            str_detect(居住地, '(釧路市|釧路町|厚岸町|浜中町|標茶町|弟子屈町|鶴居村|白糠町)'
+                                          ) ~ '釧路総合振興局',
+                            str_detect(居住地, '(室蘭市|苫小牧市|登別市|伊達市|豊浦町|壮瞥町|白老町|厚真町|洞爺湖町|安平町|むかわ町)'
+                                          ) ~ '胆振総合振興局',
+                            str_detect(居住地, '(日高町|平取町|新冠町|浦河町|様似町|えりも町|ひだか町)'
+                                          ) ~ '日高振興局',
+                            str_detect(居住地, '(函館市|北斗市|松前町|福島町|知内町|木古内町|七飯町|鹿部町|森町|八雲町|長万部町)'
+                                          ) ~ '渡島総合振興局',
+                            str_detect(居住地, '(江差町|上ノ国町|厚沢部町|乙部町|奥尻町|今金町|せたな町)'
+                                          ) ~ '檜山振興局',
+                            str_detect(居住地, '(留萌市|増毛町|小平町|苫前町|羽幌町|初山別村|遠別町|天塩町)'
+                                          ) ~ '留萌振興局',
+                            str_detect(居住地, '(稚内市|猿払村|浜頓別町|中頓別町|枝幸町|豊富町|礼文町|利尻町|利尻富士町|幌延町)'
+                                          ) ~ '宗谷総合振興局',
+                            str_detect(居住地, '(北見市|網走市|紋別市|美幌町|津別町|斜里町|清里町|小清水町|訓子府町|置戸町|佐呂間町|遠軽町|湧別町|滝上町|興部町|西興部村|雄武町|大空町)'
+                                          ) ~ 'オホーツク総合振興局',
+                            str_detect(居住地, '(帯広市|音更町|士幌町|上士幌町|鹿追町|新得町|清水町|芽室町|中札内村|更別村|大樹町|広尾町|幕別町|池田町|豊頃町|本別町|足寄町|陸別町|浦幌町)'
+                                          ) ~ '十勝総合振興局',
+                            str_detect(居住地, '(根室市|別海町|中標津町|標津町|羅臼町)'
+                                          ) ~ '根室振興局',
+                            TRUE ~ 居住地)) %>% 
+  
+  # 公表日の変換
+  mutate(公表日 = as_date(公表日)) %>% 
+  # 都道府県の追加
+  mutate(都道府県 = '北海道') %>% select(都道府県, everything())
+
+
+
+# 保存
+write_excel_csv(x = data, path = 'data/covid19_Hokkaido.csv')
+
+
 # 千葉県 --------------------------------------------------------------
 
 scrape_res <-
@@ -227,11 +236,13 @@ data <-
   # 公表日のクリーニング
   mutate(公表日 = as_date(公表日)) %>% 
   # 年代のクリーニング
-  mutate(年代 = case_when(grepl(x = 年代, '.*就学児') ~ sub(x = 年代, '.*就学児', '0代'),
-                        TRUE ~ paste0(年代, '代'))) %>%  
+  mutate(年代 = case_when(年代 == '調査中' ~ NA_character_,
+                          grepl(x = 年代, '.*就学児') ~ sub(x = 年代, '.*就学児', '0代'),
+                          年代 == '100' ~ '90代',
+                          TRUE ~ paste0(年代, '代'))) %>%  
   # 性別のクリーニング
-  # 今のところなし
-  
+  mutate(性別 = case_when(grepl(x = 性別, '^[男女]$') ~ paste0(性別, '性'),
+                           TRUE ~ 性別)) %>% 
   # 居住地のクリーニング
   mutate(居住地 = case_when(grepl(x = 居住地, '[市町村]') ~ 居住地,
                          grepl(x = 居住地, '府外') ~ '県外',
@@ -295,7 +306,7 @@ write_excel_csv(data, 'data/covid19_Kanagawa.csv')
 
 
 # 兵庫県 ---------------------------------------------------------------------
-download.file(url = 'https://web.pref.hyogo.lg.jp/kk03/documents/corona-kanjajokyou.xlsx',
+download.file(url = 'https://web.pref.hyogo.lg.jp/kk03/documents/corona-kanjyajyokyo.xlsx',
               destfile = 'data/covid19_Hyogo.xlsx', 
               mode = 'wb')
 
